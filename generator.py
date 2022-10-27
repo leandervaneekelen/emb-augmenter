bbf08a641e041ea0ba374d91adf89ba9af3215a1
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2022-10-27 19:23:12 -0400

    Added files for testing IndependentFast times

---
+++ generator.py
@@ -0,0 +1,240 @@
+from cgi import test
+import math
+
+from torch import nn, Tensor
+import torch
+import torch.nn as nn
+from torch.nn import ReLU, ELU
+
+
+class GeneratorMLP(nn.Module):
+    def __init__(self, n_tokens=1024, dropout=0.0) -> None:
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(n_tokens * 2, n_tokens),
+            ELU(),
+            nn.AlphaDropout(dropout),
+            nn.Linear(n_tokens, n_tokens // 2),
+            ELU(),
+            nn.AlphaDropout(dropout),
+            nn.Linear(n_tokens // 2, n_tokens // 4),
+            ELU(),
+            nn.AlphaDropout(dropout),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(n_tokens // 4, n_tokens // 2),
+            ELU(),
+            nn.AlphaDropout(dropout),
+            nn.Linear(n_tokens // 2, n_tokens // 2),
+            ELU(),
+            nn.AlphaDropout(dropout),
+            nn.Linear(n_tokens // 2, n_tokens),
+        )
+
+    def forward(self, x, z):
+        """
+        Forward pass.
+            Args:
+                - x: original feature [B, 1024 x 1]
+                - z: random noise [B, 1024 x1]
+            Returns:
+                - x_aug: augmented version of x [B, 1024]
+        """
+        x_aug = torch.cat([x, z], dim=1).squeeze()
+        x_aug = self.encoder(x_aug)
+        x_aug = self.decoder(x_aug)
+
+        return x_aug
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 1024):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
+        )
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe
+        return self.dropout(x)
+
+
+class GeneratorTransformer(nn.Module):
+    def __init__(
+        self,
+        n_tokens=1024,
+        n_heads=1,
+        emb_dim=8,
+        dropout=0.0,
+    ):
+        super(GeneratorTransformer, self).__init__()
+
+        self.augment_channel = nn.Linear(2, emb_dim)
+        self.pos_encoding = PositionalEncoding(
+            d_model=emb_dim, dropout=dropout, max_len=n_tokens
+        )
+        self.transformer = nn.TransformerEncoderLayer(
+            d_model=emb_dim,
+            dim_feedforward=emb_dim,
+            nhead=n_heads,
+            dropout=dropout,
+            batch_first=True,
+        )
+        self.reduce_channel = nn.Linear(emb_dim, 1)
+
+    def forward(self, x, z):
+        """
+        Forward pass.
+            Args:
+                - x: original feature [B x 1024 x 1]
+                - z: random noise [B x 1024 x1]
+            Returns:
+                - x_aug: augmented version of x [B x 1024]
+        """
+        x, z = x.unsqueeze(2), z.unsqueeze(2)
+        x_aug = torch.cat([x, z], dim=2)  # concat original and noise
+        x_aug = torch.permute(x_aug, (1, 0, 2))  # seq len first
+        x_aug = self.augment_channel(x_aug)  # increast channel from 2 to emb_dim
+        x_aug = self.pos_encoding(x_aug)  # position encoding
+        x_aug = self.transformer(x_aug)  # apply transformer
+        x_aug = self.reduce_channel(x_aug)  # reduce channel
+        x_aug = torch.permute(x_aug.squeeze(dim=2), (1, 0))  # batch first
+        return x_aug
+
+
+class GeneratorIndependent(nn.Module):
+    def __init__(
+        self,
+    ):
+        super(GeneratorIndependent, self).__init__()
+
+        self.all_mlps = []
+        for _ in range(1024):
+            mlp = nn.Sequential(nn.Linear(2, 4), nn.ReLU(), nn.Linear(4, 1))
+            # mlp = nn.Sequential(
+            #     nn.Linear(2, 1),
+            # )
+            self.all_mlps.append(mlp)
+        self.all_mlps = nn.ModuleList(self.all_mlps)
+
+    def forward(self, x, z):
+        """
+        Forward pass.
+            Args:
+                - x: original feature [B x 1024 x 1]
+                - z: random noise [B x 1024 x1]
+            Returns:
+                - x_aug: augmented version of x [B x 1024]
+        """
+        x, z = x.unsqueeze(2), z.unsqueeze(2)
+        data = torch.cat([x, z], dim=2)  # concat original and noise
+        data = torch.permute(data, (1, 0, 2))  # seq len first
+        augmentations = []
+        for i in range(1024):
+            o = self.all_mlps[i](data[i, :, :])
+            augmentations.append(o)
+        augmentations = torch.cat(augmentations, dim=1)
+        return augmentations
+
+
+class GeneratorIndependentFast(nn.Module):
+    def __init__(
+        self,
+    ):
+        super(GeneratorIndependentFast, self).__init__()
+
+        # self.mlp = nn.Sequential(
+        #     nn.Linear(2, 32),
+        #     nn.ReLU(),
+        #     nn.Linear(32, 8),
+        #     nn.ReLU(),
+        #     nn.Linear(8, 1)
+        # )
+
+        self.mlp = nn.Sequential(
+            nn.Linear(2, 64),
+            nn.ReLU(),
+            nn.Linear(64, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+
+        # self.mlp = nn.Sequential(
+        #     nn.Linear(2, 1, bias=False),
+        # )
+
+    def forward(self, x, z):
+        """
+        Forward pass.
+            Args:
+                - x: original feature [B x 1024 x 1]
+                - z: random noise [B x 1024 x1]
+            Returns:
+                - x_aug: augmented version of x [B x 1024]
+        """
+        x, z = x.unsqueeze(2), z.unsqueeze(2)
+        data = torch.cat([x, z], dim=2)  # concat original and noise
+        data = torch.permute(data, (1, 0, 2))  # seq len first
+        # augmentations = []
+        # for i in range(1024):
+        #     o = self.mlp(data[i, :, :])
+        #     augmentations.append(o)
+        # augmentations = torch.cat(augmentations, dim=1)
+        # print('Data', data.shape)
+        augmentations = self.mlp(data)
+        augmentations = torch.permute(augmentations, (1, 0, 2)).squeeze()
+        # print('Augmentations', augmentations.shape)
+        # exit()
+
+        return augmentations
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+if __name__ == "__main__":
+
+    test_independent = True
+    if test_independent:
+        net = GeneratorIndependent().cuda()
+        print("Number of parameters:", count_parameters(net))
+        x = torch.randn(32, 1024).cuda()
+        z = torch.randn(32, 1024).cuda()
+        out = net(x, z)
+        print("Out shape:", out.shape)
+
+    test_mlp = False
+    if test_mlp:
+        net = GeneratorMLP().cuda()
+        # print(net)
+        print("Number of parameters:", count_parameters(net))
+        x = torch.randn(32, 1024).cuda()
+        z = torch.randn(32, 1024).cuda()
+        out = net(x, z)
+        print("Out shape:", out.shape)
+
+    test_transformer = False
+    if test_transformer:
+        net = GeneratorTransformer(n_heads=4, emb_dim=64).cuda()
+        # print("Net:", net)
+        print("Number of parameters:", count_parameters(net))
+        x = torch.randn(32, 1024).cuda()
+        z = torch.randn(32, 1024).cuda()
+        out = net(x, z)
+        print("Out shape:", out.shape)
---
+++ test_aug_times.py
@@ -0,0 +1,109 @@
+import os
+import torch
+import numpy as np
+
+from augmentations.augmentations import aug_combined, aug_rotation
+
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+torch.autograd.set_grad_enabled(False)
+
+from torchvision import transforms
+from utils.resnet_custom import resnet50_baseline
+
+def eval_transforms(pretrained=False):
+    if pretrained:
+        mean = (0.485, 0.456, 0.406)
+        std = (0.229, 0.224, 0.225)
+
+    else:
+        mean = (0.5, 0.5, 0.5)
+        std = (0.5, 0.5, 0.5)
+
+    trnsfrms_val = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)]
+    )
+
+    return trnsfrms_val
+
+resnet = resnet50_baseline(pretrained=True)
+resnet.to(device)
+resnet.eval()
+
+roi_transforms = eval_transforms(pretrained=True)
+
+import augmentations.augmentations as A
+
+# import generator
+from generator import GeneratorMLP, GeneratorIndependent, GeneratorIndependentFast
+
+# load model
+# dagan_run_code = "gan_mlp_s1_lr1e-03_None_b64_20221909_182052" # old
+# dagan_run_code = "gan_mlp_s1_lr1e-03_None_b64_20221909_183959" # MLP exp
+# dagan_run_code = "gan_independent_s1_lr1e-03_None_b64_20221909_222224" # MLP ind
+dagan_run_code = "gan_independent_fast_s1_lr1e-03_None_b64_20222009_004136" # MLP ind fast
+dagan_state_path = f"/home/guillaume/Documents/uda/project-augmented-embeddings/2-dagan/results/sicapv2/{dagan_run_code}/s_4_checkpoint.pt"
+dagan_state_dict = torch.load(dagan_state_path)
+
+n_tokens = 1024
+dropout = 0.2
+# generator = GeneratorMLP(n_tokens, dropout)
+# generator = GeneratorIndependent()
+generator = GeneratorIndependentFast()
+generator.load_state_dict(dagan_state_dict["G_state_dict"])
+generator.eval().to(device)
+# print(generator)
+
+def get_true_emb(img, b):
+    true_emb = []
+    for _ in range(b):
+        aug_img = A.aug_rotation(img)
+        aug_img = roi_transforms(aug_img).unsqueeze(0)
+        aug_img = aug_img.to(device)
+        emb = resnet(aug_img)
+        true_emb.append(emb)
+    
+    true_emb = torch.stack(true_emb, dim=0).to(device)
+    return true_emb
+
+def get_gen_emb(b):
+    with torch.no_grad():
+        emb = torch.randn(b, 1024, requires_grad=False).to(device)
+        noise = torch.randn(emb.size(0), emb.size(1), requires_grad=False).to(device)
+        aug_embs = generator.forward(emb, noise)
+    
+    return aug_embs
+
+def time_func(f):
+    st = time.time()
+    f()
+    et = time.time()
+    res = et - st
+    return res
+
+if __name__ == "__main__":
+    import time
+
+    n = int(input("Enter number of times to run: "))
+    b = int(input("Enter batch size: "))
+    
+    time_true = 0
+    time_gen = 0
+    for i in range(n):
+        img = np.random.randint(low=0, high=255, size=(256,256,3), dtype='uint8')
+        t_true = time_func(lambda: get_true_emb(img, b))
+        time_true += t_true
+        t_gen = time_func(lambda: get_gen_emb(b))
+        time_gen += t_gen
+        print(f"{i}/{n}")
+        print(t_true)
+        print(t_gen)
+        
+    time_true /= n
+    time_gen /= n
+    gain = time_true / time_gen
+
+    print("true:        ", time_true)
+    print("gen (mlp):   ", time_gen)
+    print("gain:        ", gain)
